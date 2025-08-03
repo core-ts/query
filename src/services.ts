b61bfba124b85656a229a61911266168f80897a1
Duc Nguyen <minhduc140583@gmail.com>
2025-08-03 22:57:26 +0700

    Refactor DB

--- src/services.ts
+++ src/services.ts
@@ -289,7 +289,7 @@ export interface Manager {
   driver: string
   param(i: number): string
   exec(sql: string, args?: any[], ctx?: any): Promise<number>
-  // execBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>
+  execBatch(statements: Statement[], firstSuccess?: boolean, ctx?: any): Promise<number>
   query<T>(sql: string, args?: any[], m?: StringMap, bools?: Attribute[], ctx?: any): Promise<T[]>
 }
 export type DB = Manager
